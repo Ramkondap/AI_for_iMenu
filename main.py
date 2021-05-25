0809fcc5bf660af400e7ed8f98d88b3d0f5c121b
DilrajS <49254698+DilrajS@users.noreply.github.com>
2021-05-24 17:36:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,24 +6,61 @@ Restaurant.website = "website.com"
 Restaurant.schedule = {"weekday": "8 AM to 9 PM",
                        "weekend": "9 AM to 10 PM"}
 
-
 no_set = {"no", "nope", "nah", "not right now", "no thanks", "not this time"}
 yes_set = {"yes", "yup", "yeah", "yea", "ya", "yas", "yep", "totally", "sure"}
 hours_set = {"open", "closed", "close", "time"}
 
 
 def output_tts(reply):
-    print(reply)  # need to change this to TTS.
+    print(reply)
+    # [START TTS]
+    '''
+    from google.cloud import texttospeech
+
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
+
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.SynthesisInput(text=reply)
+
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
+
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    # The response's audio_content is binary.
+    with open("output.mp3", "wb") as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
+    # [END TTS]
     
-    
+    # [PLAY AUDIO] 
+    from playsound import playsound
+    playsound('output.mp3')
+'''
+
+
 def greeting():
     output_tts("Thank you for calling " + Restaurant.name + "Would you like to place an order?")
     initial_response = input().lower()
     if initial_response in yes_set:
         carryout_or_delivery()
     else:
-        for i in initial_response:
-            if initial_response[i] in hours_set:
+        for _ in initial_response:  # Might need to edit this 
+            if initial_response.__contains__(hours_set):
                 output_tts("We are open from " + Restaurant.schedule.get("weekday") + "on the weekdays and "
                            + Restaurant.schedule.get("weekend") + "on the weekends.")
             else:
